b7d5cd7047f739bd9a238db77b26b076c56661fa
sandra kinoti <sandra.kinoti@student.moringaschool.com>
2022-05-29 22:21:14 +0300

    add imports in models.py

--- galleria/models.py
+++ galleria/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+from django.template.defaultfilters import slugify
+from django_resized import ResizedImageField
+from django.utils import timezone
+from uuid import uuid4
+from django.urls import reverse
