5c8577c49ee91574649335ffa70fdb904b14091d
sandra kinoti <sandra.kinoti@student.moringaschool.com>
2022-05-29 23:17:23 +0300

    add register models to admin.py

--- galleria/admin.py
+++ galleria/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Location
+from .models import Location,Category,Image
 # Register your models here.
-from django.contrib import admin
-from .models import Location
 admin.site.register(Location)
+admin.site.register(Category)
+admin.site.register(Image)
