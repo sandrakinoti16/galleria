1ffd3b8c0508806fa7195c7195ebb529e64d72d8
sandra kinoti <sandra.kinoti@student.moringaschool.com>
2022-05-29 22:22:42 +0300

    add category model

--- galleria/models.py
+++ galleria/models.py
@@ -6,3 +6,34 @@ from django_resized import ResizedImageField
 from django.utils import timezone
 from uuid import uuid4
 from django.urls import reverse
+
+class Category(models.Model):
+    title = models.CharField(null=True, blank=True, max_length=200)
+
+    #Utility Variable
+    uniqueId = models.CharField(null=True, blank=True, max_length=100)
+    slug = models.SlugField(max_length=500, unique=True, blank=True, null=True)
+    date_created = models.DateTimeField(blank=True, null=True)
+    last_updated = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return '{} {}'.format(self.title, self.uniqueId)
+
+
+    def get_absolute_url(self):
+        return reverse('category-detail', kwargs={'slug': self.slug})
+
+
+    def save(self, *args, **kwargs):
+        if self.date_created is None:
+            self.date_created = timezone.localtime(timezone.now())
+        if self.uniqueId is None:
+            self.uniqueId = str(uuid4()).split('-')[4]
+            self.slug = slugify('{} {}'.format(self.title, self.uniqueId))
+
+
+        self.slug = slugify('{} {}'.format(self.title, self.uniqueId))
+        self.last_updated = timezone.localtime(timezone.now())
+        super(Category, self).save(*args, **kwargs)
+
+
